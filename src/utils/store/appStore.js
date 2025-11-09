d542aebdee91980884883d6732bb1bde2f1642c2
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-09 14:02:32 +0530

    2.3.1 - created sidebarMenuSlice for redux store

--- src/utils/store/appStore.js
+++ src/utils/store/appStore.js
@@ -1,6 +1,9 @@
 // appStore stores app data.  i.e, global object
 import { configureStore } from "@reduxjs/toolkit";
+import sidebarMenuReducer from "./sidebarMenuSlice.js";
 
-const appStore = configureStore({}); // creates a store with given Slices
+const appStore = configureStore({
+  reducer: { sidebarMenu: sidebarMenuReducer },
+}); // creates a store with given Slices
 
 export default appStore;
---
+++ src/utils/store/sidebarMenuSlice.js
@@ -0,0 +1,15 @@
+// slice contains particial data of Redux Store.  i.e, Sidebar menu data
+import { createSlice } from "@reduxjs/toolkit";
+
+const sidebarMenuSlice = createSlice({
+  name: "sidebarMenu",
+  initialState: { isMenuOpen: true },
+  reducers: {
+    toggleMenu: (state) => {
+      state.isMenuOpen = !state.isMenuOpen;
+    },
+  },
+}); // creates a slice
+
+export const { toggleMenu } = sidebarMenuSlice.actions;
+export default sidebarMenuSlice.reducer;
