ee6af8b9e4c742685a10e0364542cbe65b77bf33
Kumar <kumarnaidukarri2002@gmail.com>
2025-11-16 23:12:47 +0530

    10 - Implemented Feature * 'Adding Chat Message' using Input box

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,6 +1,6 @@
 // LiveChat component contains 'Live Chatting Messages'.
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/store/chatSlice.js";
 
@@ -10,6 +10,8 @@ import { generateRandomName, generateRandomMessage } from "../utils/helper.js";
 import ChatMessage from "./ChatMessage.jsx";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState(""); // local state variable
+
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages); // Subscribing to Chat Slice of Store
@@ -26,20 +28,45 @@ const LiveChat = () => {
           message: generateRandomMessage() + " 🚀",
         })
       );
-    }, 1000);
+    }, 2000);
 
     // Cleanup fn
     return () => clearInterval(i);
   }, []);
 
   return (
-    <div className="w-full h-[600px] ml-2 p-2 bg-slate-100 border border-black rounded-lg overflow-y-scroll  flex flex-col-reverse">
-      {/* Iterating on ChatMessages data from redux store */}
-      {chatMessages.map((cm, index) => {
-        // Disclaimer: Don't use indexes as keys
-        return <ChatMessage name={cm.name} message={cm.message} key={index} />;
-      })}
-    </div>
+    <>
+      <div className="w-full h-[600px] ml-2 p-2 bg-slate-100 border border-black rounded-lg overflow-y-scroll  flex flex-col-reverse">
+        {/* Iterating on ChatMessages data from redux store */}
+        {chatMessages.map((cm, index) => {
+          // Disclaimer: Don't use indexes as keys
+          return (
+            <ChatMessage name={cm.name} message={cm.message} key={index} />
+          );
+        })}
+      </div>
+
+      <form
+        className="form-container  w-full p-2 ml-2 mt-2 border border-black"
+        onSubmit={(e) => {
+          e.preventDefault(); // prevents default form behaviour and stops page reloads.
+
+          dispatch(addMessage({ name: "Kumar", message: liveMessage })); // dispatch an Action
+
+          setLiveMessage(""); // clears the input box
+        }}
+      >
+        <input
+          className="w-96 px-2 py-1 bg-gray-200"
+          type="text"
+          value={liveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className="mx-2 px-4 py-1 bg-green-100"> Send </button>
+      </form>
+    </>
   );
 };
 /*
